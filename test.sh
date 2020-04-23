a530b31114dc832f3f38522e32b4c08ee6dc45b0
ramagcpgrafana <54090005+ramagcpgrafana@users.noreply.github.com>
2020-04-23 12:19:25 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo $FIRST
-echo $SECOND
-#num=$(( 10 + 20 ))
-num=$(( $FIRST + $SECOND ))
+#echo $FIRST
+#echo $SECOND
+num=$(( 10 + 20 ))
+#num=$(( $FIRST + $SECOND ))
 echo total is $num
 echo "testing webhook"
